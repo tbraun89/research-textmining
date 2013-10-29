33f14f6cecf25b90035b2886a287f43fa2e794e6
tbraun <tbraun@tnt-web-solutions.de>
2013-10-29 11:59:35 +0100

    added initial maven project with junit4

---
+++ src/main/java/de/htwdd/fs/textmining/rdf/CorporaGenerator.java
@@ -0,0 +1,9 @@
+package de.htwdd.fs.textmining.rdf;
+
+public class CorporaGenerator {
+
+    public static void main(String args[]) {
+
+    }
+
+}
---
+++ src/test/java/de/htwdd/fs/textmining/rdf/CorporaGeneratorTest.java
@@ -0,0 +1,8 @@
+package de.htwdd.fs.textmining.rdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CorporaGeneratorTest {
+
+}
